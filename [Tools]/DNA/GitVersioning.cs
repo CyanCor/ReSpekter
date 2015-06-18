6cc49aa97f82ea0c49e5f441a3816addff837717
Michael Loeffl <loeffl@cyancor.com>
2015-06-18 12:55:43 +0200

    Updated versioning system.

--- [Tools]/DNA/GitVersioning.cs
+++ [Tools]/DNA/GitVersioning.cs
@@ -26,71 +26,98 @@ namespace CyanCor.SourceControl
 {
     public class Git
     {
+        
+        public string Hash { get; set; }
+        public string ShortHash { get; set; }
+        public string ReleaseType { get; set; }
+
+        public string Prefix { get; set; }
+        public int Major { get; set; }
+        public int Minor { get; set; }
+        public int Patch { get; set; }
+        public int CommitsAfterTag { get; set; }
+        public string Postfix { get; set; }
+        public string Version { get; set; }
+        public string PlainVersion { get; set; }
+        public string ShortVersion { get; set; }
+
         public Git()
         {
             GetLastCommitInformation();
-            GetTagInformation();
+            int branchMajor, branchMinor, branchPatch;
+            var branch = ReadBranch(out branchMajor, out branchMinor, out branchPatch);
+            int tagMajor, tagMinor, tagPatch, tagCommits;
+            ReadTagVersion(out tagMajor, out tagMinor, out tagPatch, out tagCommits);
+
+            if (branch.Equals("master"))
+            {
+                ReleaseType = "production";
+                Major = tagMajor;
+                Minor = tagMinor;
+                Patch = tagPatch;
+                CommitsAfterTag = tagCommits;
+                Postfix = string.Empty;
+            }
+            else if (branch.StartsWith("release-"))
+            {
+                ReleaseType = "beta";
+                Major = branchMajor;
+                Minor = branchMinor;
+                Patch = branchPatch;
+                CommitsAfterTag = tagCommits;
+                Postfix = "." + CommitsAfterTag + "-" + ShortHash;
+            }
+            else if (branch.Equals("develop"))
+            {
+                ReleaseType = "dev";
+                Major = tagMajor;
+                Minor = tagMinor;
+                Patch = tagPatch;
+                CommitsAfterTag = tagCommits;
+                Postfix = "." + tagCommits + "-" + ShortHash;
+            }
+            else
+            {
+                ReleaseType = "feature";
+                Major = tagMajor;
+                Minor = tagMinor;
+                Patch = tagPatch;
+                CommitsAfterTag = tagCommits;
+                Postfix = "." + tagCommits + "-" + ShortHash;
+            }
+
+            Version = ReleaseType + "-" + Major + "." + Minor + "." + Patch + Postfix;
+            ShortVersion = Major + "." + Minor + "." + Patch;
+            PlainVersion = ShortVersion + "." + CommitsAfterTag;
         }
 
-        public string Branch
+        string ReadBranch(out int branchMajor, out int branchMinor, out int branchPatch)
         {
-            get
-            {
-                string output;
-                string error;
-                Tools.CommandLineExecute("git", "branch", out output, out error);
+            var branch = ReadBranch();
+            int commit;
+            ExtractVersion(branch, out branchMajor, out branchMinor, out branchPatch, out commit);
+            return branch;
+        }
 
-                if (string.IsNullOrEmpty(output))
+        string ReadBranch()
+        {
+            string output;
+            string error;
+            Tools.CommandLineExecute("git", "branch", out output, out error);
+
+            if (string.IsNullOrEmpty(output))
+            {
+                return string.Empty;
+            }
+            else
+            {
+                string[] branches = output.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                string result = branches.First((s) => s.Contains('*'));
+                if (string.IsNullOrEmpty(result))
                 {
                     return string.Empty;
                 }
-                else
-                {
-                    string[] branches = output.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
-                    string result = branches.First((s) => s.Contains('*'));
-                    if (string.IsNullOrEmpty(result))
-                    {
-                        return string.Empty;
-                    }
-                    return result.Trim(new[] { '\t', ' ', '*', '\n', '\r' });
-                }
-            }
-        }
-		
-        public string Hash { get; private set; }
-        public string ShortHash { get; private set; }
-        public string Author { get; private set; }
-        public string AuthorEmail { get; private set; }
-        public DateTime AuthorDate { get; private set; }
-        public string Committer { get; private set; }
-        public string CommitterEmail { get; private set; }
-        public DateTime CommitterDate { get; private set; }
-        public string Subject { get; private set; }
-		public string Version { get; private set; }
-		public int CommitsAfterTag { get; private set; }
-        public string ReleaseType { get; private set; }
-        public int Major { get; private set; }
-        public int Minor { get; private set; }
-        public int Patch { get; private set; }
-
-
-        internal string CommitterDateString
-        {
-            set
-            {
-                DateTime dt;
-                DateTime.TryParse(value, out dt);
-                CommitterDate = dt;
-            }
-        }
-
-        internal string AuthorDateString
-        {
-            set
-            {
-                DateTime dt;
-                DateTime.TryParse(value, out dt);
-                AuthorDate = dt;
+                return result.Trim(new[] { '\t', ' ', '*', '\n', '\r' });
             }
         }
 		
@@ -100,14 +127,7 @@ namespace CyanCor.SourceControl
             string error;
 			
 			string format = "Hash:%H%n" + 
-							"ShortHash:%h%n" + 
-							"Author:%an%n" +
-							"AuthorEmail:%ae%n" +
-							"AuthorDateString:%ai%n" +
-							"Committer:%cn%n" +
-							"CommitterEmail:%ce%n" +
-							"CommitterDateString:%ci%n" +
-							"Subject:%s%n";
+							"ShortHash:%h%n";
 			
             int returnCode = Tools.CommandLineExecute("git", "log -n 1 --format=" + format, out output, out error);
 
@@ -136,8 +156,39 @@ namespace CyanCor.SourceControl
 				property.SetValue(this, value, null);
 			}
 		}
+
+        public string ReadTagVersion(out int major, out int minor, out int patch, out int commit)
+        {   
+            var version = GetTagInformation();
+            Console.WriteLine(version);
+            
+            ExtractVersion(version, out major, out minor, out patch, out commit);
+            return null;
+        }
+
+        public void ExtractVersion(string version, out int major, out int minor, out int patch, out int commit)
+        {
+            var reg = new Regex("^(([a-z]+)-)?([0-9]+)\\.([0-9]+)([^0-9a-zA-Z]([0-9]+))?([^0-9a-zA-Z]([0-9]+))?");
+            var match = reg.Match(version);
+
+            if (match.Success)
+            {
+                // var buildType = match.Groups[2].Value;
+                int.TryParse(match.Groups[3].Value, out major);
+                int.TryParse(match.Groups[4].Value, out minor);
+                int.TryParse(match.Groups[6].Value, out patch);
+                int.TryParse(match.Groups[8].Value, out commit);
+            }
+            else
+            {
+                major = 0;
+                minor = 0;
+                patch = 0;
+                commit = 0;
+            }
+        }
 		
-		private void GetTagInformation()
+		private string GetTagInformation()
 		{
             string output;
             string error;
@@ -146,46 +197,18 @@ namespace CyanCor.SourceControl
 
 		    if (returnCode != 0 || string.IsNullOrWhiteSpace(output))
 		    {
+                var commits = 0;
+                returnCode = Tools.CommandLineExecute("git", "rev-list --count HEAD", out output, out error);
+                if (returnCode == 0 && !string.IsNullOrWhiteSpace(output))
+                {
+                    int.TryParse(output, out commits);
+                }
+
                 Console.WriteLine("Failed to get tag information. Possible causes: No tag has been set yet; Git is not installed or not in PATH; This is not a git repository.");
-		        output = "dev-0.0.0-0-g" + ShortHash;
+		        output = "dev-0.0.0-" + commits + "-g" + ShortHash;
 		    }
 
-            Regex reg = new Regex("^([a-z]+)-([0-9]+)\\.([0-9]+)\\.([0-9]+)(-([0-9]+)-g([0-9a-fA-F]+))?$");
-
-            var match = reg.Match(output.Trim());
-            int major = 0;
-            int minor = 0;
-            int patch = 0;
-            int commits = 0;
-		    if (match.Success)
-		    {
-		        Version = match.Groups[0].Value;
-
-                ReleaseType = match.Groups[1].Value;
-		        int.TryParse(match.Groups[2].Value, out major);
-                int.TryParse(match.Groups[3].Value, out minor);
-                int.TryParse(match.Groups[4].Value, out patch);
-                int.TryParse(match.Groups[6].Value, out commits);
-
-		        Major = major;
-		        Minor = minor;
-		        Patch = patch;
-		        CommitsAfterTag = commits;
-		    }
-		    else
-		    {
-		        Version = output;
-                Console.WriteLine("Error: Invalid Tag!");
-                ReleaseType = "unknown";
-		    }
-
-            Console.WriteLine("Release: " + ReleaseType);
-            Console.WriteLine("Major: " + Major);
-            Console.WriteLine("Minor: " + Minor);
-            Console.WriteLine("Patch: " + Patch);
-            Console.WriteLine("CommitsAfterTag:" + CommitsAfterTag);
-            Console.WriteLine(match.Groups[7]);
-            Console.WriteLine(Hash);
+            return output;
 		}
     }
 }
