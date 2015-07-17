5a13176338acc23ce50825284fb26bdb489814c8
Michael Loeffl <loeffl@cyancor.com>
2015-07-17 12:38:35 +0200

    Minor change.

--- ReSpekter/Helper/TemplatePropertyTransformator.cs
+++ ReSpekter/Helper/TemplatePropertyTransformator.cs
@@ -1,14 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
-using Mono.Cecil;
-using Mono.Cecil.Cil;
-using Mono.Cecil.Rocks;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TemplatePropertyTransformator.cs" company="CyanCor GmbH">
+//   Copyright (c) 2015 CyanCor GmbH
+//   
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//   
+//   http://www.apache.org/licenses/LICENSE-2.0
+//   
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and limitations under the License.
+// </copyright>
+// <summary>
+//   Defines the TemplatePropertyTransformator type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace CyanCor.ReSpekter.Helper
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using Mono.Cecil;
+    using Mono.Cecil.Cil;
+
     public class TemplatePropertyTransformator
     {
         private PropertyDefinition _templateProp;
@@ -30,7 +48,7 @@ namespace CyanCor.ReSpekter.Helper
             _target = target;
             _template = target.Module.Import(me.Member.DeclaringType).Resolve();
             _templateProp = _template.Properties.Single(definition => definition.Name.Equals(me.Member.Name));
-
+            
             if (target.GetMethod != null)
             {
                 if (_templateProp.GetMethod != null)
